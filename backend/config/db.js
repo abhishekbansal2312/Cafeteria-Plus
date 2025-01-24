19fe67076d1258ea871c0e61e2c52461227edf46
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-01-24 11:08:40 +0530

    `Database connection established`

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((conn) => {
+    console.log(`MongoDB connected}`);
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  });
+
+module.exports = {};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+require("./config/db");
 const app = express();
 
 app.use(express.json());
